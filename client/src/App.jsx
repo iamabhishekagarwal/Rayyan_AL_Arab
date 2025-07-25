6912dfcfa54b0b7c360f138893b1f8631f361ec7
adityamishra139 <aditya2vibha@gmail.com>
2025-07-25 21:24:42 +0530

    fe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import {Route,Routes,BrowserRouter} from 'react-router-dom'
 import Maintenance from './pages/Maintenance'
 import Home from './pages/Home'
+import Aboutus from "./pages/Aboutus"
+import Projects from './pages/Projects'
+import Services from './pages/Services'
+import ContactUs from './pages/ContactUs'
 function App() {
 
   return (
@@ -8,6 +12,10 @@ function App() {
       <Routes>
         <Route path='/maintenance' element={<Maintenance></Maintenance>} />
         <Route path='/' element={<Home></Home>}/>
+        <Route path='/about' element={<Aboutus></Aboutus>}/>
+        <Route path='/projects' element={<Projects></Projects>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/contact' element={<ContactUs></ContactUs>}></Route>
       </Routes>
    </BrowserRouter>
   )
